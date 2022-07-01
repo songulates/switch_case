5495800a194e9d4412120ce88f75fa225f8bf991
songulates <songl.ates.02@gmail.com>
2022-07-01 16:11:50 +0300

    case

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+namespace console_programlama
+{ 
+    class Program
+    {
+        static void Main(string[] args)//
+        {
+            int month=DateTime.Now.Month;
+            switch(month)
+            {
+                case 1:
+                Console.WriteLine("ocak");
+                break;
+                case 2:
+                Console.WriteLine("subat");
+                break;
+                case 3:
+                Console.WriteLine("mart");
+                break;
+                case 7:
+                Console.WriteLine("temmuz");
+                break;
+                default:
+                break;
+
+            } 
+        }
+    }
+}
